7028d21668f11b3fd48a53e0bcb58e922c60453c
Martin Murphy <martin.murphy@whiteboard-it.com>
2017-09-30 14:43:52 -0500

    Initial Commit

---
+++ HelloWorldSteriods/Joke.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace HelloWorldSteriods
+{
+    public class Joke
+    {
+        public string Value { get; set; }
+        public  string Url { get; set; }
+        [JsonProperty("icon_url")]
+        public string IconUrl { get; set; }
+        public string Id { get; set; }
+    }
+}
---
+++ HelloWorldSteriods/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Speech.Synthesis;
+using Newtonsoft;
+using Newtonsoft.Json;
+
+namespace HelloWorldSteriods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var joke =GetJoke();
+            Speak(joke.Value);
+        }
+
+        private static Joke GetJoke()
+        {
+            var request = HttpWebRequest.Create("https://api.chucknorris.io/jokes/random");
+            var response = request.GetResponse();
+            using (var stream = new StreamReader(response.GetResponseStream()))
+            {
+                var jokeStr = stream.ReadToEnd();
+                return JsonConvert.DeserializeObject<Joke>(jokeStr);
+            }
+        }
+
+        private static void Speak(string textToSpeak)
+        {
+            var syn = new SpeechSynthesizer();
+            syn.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Senior);
+            syn.Speak(textToSpeak);
+            Console.ReadLine();
+        }
+    }
+}
